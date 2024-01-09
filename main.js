1dc482d8ec71c9321936042ffa4aa51ce639d588
Kipronokirui <victorkipronokirui2@gmail.com>
2024-01-09 20:10:02 +0300

    Assignment Complete

--- main.js
+++ main.js
@@ -3,9 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const heartIcons = document.querySelectorAll('.like-glyph');
 
+function handleHeartClick(event) {
+  const heartIcon = event.target;
 
+  mimicServerCall()
+    .then(() => {
+      if (heartIcon.textContent === EMPTY_HEART) {
+        heartIcon.textContent = FULL_HEART;
+        heartIcon.classList.add('activated-heart');
+      } else {
+        heartIcon.textContent = EMPTY_HEART;
+        heartIcon.classList.remove('activated-heart');
+      }
+    })
+    .catch((error) => {
+      const errorModal = document.getElementById('modal');
+      const errorMessage = document.getElementById('modal-message');
+      
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+      
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
 
+// Attach click event listener to each heart icon
+heartIcons.forEach(heartIcon => {
+  heartIcon.addEventListener('click', handleHeartClick);
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
